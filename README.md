126d2e9e22ad261ece643659b2dab4b23f652e30
karan51ngh <karansingh9535@gmail.com>
2025-02-14 01:57:02 +0530

    1. Updated Readme 2. Dom Manipulation

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # demxstension
+
+This project is for me to experiment with how chrome extensions work.
+
+- ###  References:
+	-  [Manifest file format](https://developer.chrome.com/docs/extensions/reference/manifest#inject-a-content-script)
+	-  [Manifest - Icons](https://developer.chrome.com/docs/extensions/reference/manifest/icons)
+	-  [Content scripts](https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts)
+	-  [Content filtering](https://developer.chrome.com/docs/extensions/develop/concepts/content-filtering)
+	-  [Samples](https://developer.chrome.com/docs/extensions/samples?text=net)
--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,19 @@
 console.log("Hello Friend!")
+const header_ul = document.querySelector("ul.global-nav__primary-items");
+const demo_ext = document.createElement("li");
+const a_demo_ext = document.createElement("a");
+
+demo_ext.classList.add('global-nav__primary-items');
+// demo_ext.innerHTML = "Test"
+
+a_demo_ext.classList.add('global-nav__primary-link--active', 'global-nav__primary-link')
+a_demo_ext.setAttribute('href', 'https://github.com/karan51ngh')
+a_demo_ext.innerHTML = "test"
+
+demo_ext.appendChild(a_demo_ext);
+header_ul.appendChild(demo_ext);
+
+const feed = document.querySelector('[aria-label="Main Feed"]');
+console.log("feed")
+console.log(feed)
+// eed-shared-update-v2__control-menu-container -> every post has this class.
